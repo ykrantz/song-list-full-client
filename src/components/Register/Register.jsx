3e4d07fc5d2a0efbbdf15fb9a3d2629307d0e505
ykrantz <ykrantz@gmail.com>
2022-02-21 17:37:08 +0200

    fix register already exsist

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,20 +5,17 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-// import { Link } from "react-router-dom";
 
-import React, { useContext, useRef } from "react";
+import React, { useContext } from "react";
 import { useState } from "react/cjs/react.development";
 import handleUser from "../../context/handleUser";
 import BackToHome from "../BackToHome/BackToHome";
 
 const Register = () => {
-  // const userName=useRef()
-  // const password=useRef()
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  // const [user,setUser]=useState({})
+  const [message, setMessege] = useState("");
   const { currentUser, setCurrentUser } = useContext(handleUser);
   const registerInServer = async () => {
     //  await   setUser({username:userName.current.value, password:password.current.value })
@@ -34,22 +31,20 @@ const Register = () => {
       body: JSON.stringify({ username: userName, password: password }),
     });
     const data = await ans.json();
-    // console.log(data);
+    console.log(data);
+    if (ans.status === 200) {
+      setCurrentUser({ username: userName, password: password });
+      localStorage.currentUser = JSON.stringify(userName);
+      localStorage.accessToken = JSON.stringify(data.accessToken);
 
-    setCurrentUser({ username: userName, password: password });
-    localStorage.currentUser = JSON.stringify(userName);
-    localStorage.accessToken = JSON.stringify(data.accessToken);
-
-    console.log("registered in server");
-    console.log(data.accessToken);
-    navigate("/");
+      console.log("registered in server");
+      console.log(data.accessToken);
+      navigate("/");
+    } else {
+      setMessege(data.message);
+    }
   };
-  // const resetInput=()=>{
-  //   setUserName("")
-  //   setPassword("")
 
-  // }
-  // console.log(user);
   return (
     <div>
       <BackToHome />
@@ -75,7 +70,6 @@ const Register = () => {
             />
           </div>
           <div>
-            {/* <TextField  value={password} onChange={(e)=>setPassword(e.target.value)} id="outlined-basic" placeholder="password" label="password" variant="outlined" /> */}
             <TextField
               type="password"
               value={password}
@@ -88,24 +82,18 @@ const Register = () => {
           </div>
         </Box>
       </div>
-      {/* <input type="text" placeholder="user name" value={userName} onChange={(e)=>setUserName(e.target.value)}></input>
-<input type="text" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}></input> */}
+
       <div className="Register-buttons">
         <Stack spacing={2} direction="row">
           <Button onClick={() => registerInServer()} variant="contained">
             Register
           </Button>
-          {/* <Button onClick={()=>resetInput()} variant="contained">Cancel</Button> */}
           <Button onClick={() => navigate("/")} variant="contained">
             Cancel
           </Button>
         </Stack>
       </div>
-      {/* <button onClick={()=>{ 
-    registerInServer()
-}
-    }
->register</button> */}
+      <p className="Register-message">{message}</p>
     </div>
   );
 };
