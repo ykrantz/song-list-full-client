2ff88a411bc790750fd7001d70ea73992b319f67
ykrantz <ykrantz@gmail.com>
2022-04-06 22:58:51 +0300

    delete unneccery imports in main page

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,20 +1,9 @@
 import "./MainPage.css";
 import { useState } from "react";
-import { useEffect } from "react";
+
 import handleMainStates from "../../context/handleMainStates";
 import Header from "./Header/Header";
 
-import PlayList from "./PlayList/PlayList";
-import PlayListsUser from "./PlayListsUser/PlayListsUser";
-
-import SearchSongs from "./SearchSongs/SearchSongs";
-import FoundedSongsYouTube from "./FoundedSongsYouTube/FoundedSongsYouTube";
-import FoundedSongsPlaylist from "./FoundedSongsPlaylist/FoundedSongsPlaylist";
-import handlePlaylist from "../../context/handlePlaylist";
-import handleSearchSongApi from "../../context/handleSearchSongApi";
-import handleSerachSongPlayList from "../../context/handleSerachSongPlayList";
-import BASE_URL from "../../general/main_var";
-import MessageNote from "../generalComponents/MessageNote/MessageNote";
 import Body from "./Body/Body";
 
 const MainPage = () => {
@@ -23,7 +12,7 @@ const MainPage = () => {
   const [newPlayList, setNewPlayList] = useState([]);
   const [userPlayLists, setUserPlayLists] = useState([]);
   const [currentPlayList, setCurrentPlayList] = useState("");
-  const [message, setMessage] = useState("");
+
   const [videoSrc, setVideoSrc] = useState(
     localStorage.youtubeId ? JSON.parse(localStorage.youtubeId) : ""
   );
