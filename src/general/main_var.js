de15c2ddbab3f7b4d4b951c63261ea87a35d678f
ykrantz <ykrantz@gmail.com>
2022-07-08 13:23:34 +0300

    add check connection to server

--- src/general/main_var.js
+++ src/general/main_var.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3008";
-// export const BASE_URL = "https://playlist-ykrantz.herokuapp.com";
+// export const BASE_URL = "http://localhost:3008";
+export const BASE_URL = "https://playlist-ykrantz.herokuapp.com";
 export const TITLE_LENGTH = 35;
 
 export const initSearchApiResults = [
